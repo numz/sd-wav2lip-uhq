4bdc85b6bb82ef3d93a9a459ce09be5bc0bd905e
Van <tangjian@msn.com>
2023-09-18 19:32:58 +0800

    auto swap dlib under macOS

--- install.py
+++ install.py
@@ -1,10 +1,15 @@
 import launch
 import os
+import platform
+
+system = platform.system()
 
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 
 with open(req_file) as file:
     for lib in file:
         lib = lib.strip()
+        if lib == "dlib-bin" and system == "Darwin":
+            lib = "dlib"  # replace dlib-bin as dlib
         if not launch.is_installed(lib):
             launch.run_pip(f"install {lib}", f"wav2lip_uhq requirement: {lib}")
