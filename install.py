76bb37079d139fc82b750a508817e8d291da4f17
Van <tangjian@msn.com>
2023-09-18 22:58:23 +0800

    ignore  onnxruntime-gpu install on macOS

--- install.py
+++ install.py
@@ -11,5 +11,7 @@ with open(req_file) as file:
         lib = lib.strip()
         if lib == "dlib-bin" and system == "Darwin":
             lib = "dlib"  # replace dlib-bin as dlib
+        if lib == "onnxruntime-gpu==1.15.0" and system == "Darwin":
+            continue  # skip onnxruntime-gpu
         if not launch.is_installed(lib):
             launch.run_pip(f"install {lib}", f"wav2lip_uhq requirement: {lib}")
